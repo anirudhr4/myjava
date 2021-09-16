81cd5075f322bd2332868149bff4a6965c639bf4
anirudhr4 <50843138+anirudhr4@users.noreply.github.com>
2021-09-16 10:44:01 +0530

    Create Simple.java

---
+++ Simple.java
@@ -0,0 +1,7 @@
+import java.io.*;
+public class Simple{
+  public static void main(String args[]){
+  System.out.println("It was mmy first java snippet in Github");
+    System.out.println("Its my second system out statement");
+  }
+}
